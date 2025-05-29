ddcd08418741ce72bf321f1ecd9bbf62caa0b6f6
Morgan Yeh <fs.morganyeh@gmail.com>
2025-05-28 22:31:19 -0400

    updated knight placement

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,26 @@ function App() {
   const [size, setSize] = useState(8);
   const [delay, setDelay] = useState(200);
   const [colour, setColour] = useState("White");
+  const [startSquare, setStartSquare] = useState([0, 0]);
 
-  const functions = [setSize, setDelay];
+  // handleSizeChange(num) sets size to num and
+  // sets startSquare to [-1, -1] if it is out of bounds
+  function handleSizeChange(num: number) {
+    setSize(num);
+
+    // check if out of bounds and reset
+    if(startSquare[0] >= num || startSquare[1] >= num) {
+      setStartSquare([-1,-1]);
+    }
+  }
+
+  const functions = [handleSizeChange, setDelay];
 
   return (<>
     <Banner/>
     <div className="body">
         <ActionBox parentCallbacks={functions} optionChanger={setColour}></ActionBox>
-        <Board size={size}/>
+        <Board size={size} startingCoord={startSquare} knightColour={colour} clickHandler={setStartSquare}/>
     </div>
     </>);
 }
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,15 +1,25 @@
 import '/src/App.css';
+import WhiteKnight from "/src/assets/knight-white.png";
+import BlackKnight from "/src/assets/knight-black.png";
+import { useState } from 'react';
 
 interface Props {
     size: number;
+    startingCoord: Array<number>;
+    knightColour: string;
+    clickHandler: (pos: Array<number>) => void;
+    
 }
 
-export default function Board( {size} : Props) {
+export default function Board( {size, startingCoord, knightColour, clickHandler} : Props) {
+    // CSS classes for styling
     var classes = "square size-" + size;
     const light = {backgroundColor: "#eeeed2" };
     const dark = {backgroundColor: "#769656" };
+
     let squares = makeChessBoard();
 
+    // makeChessBoard displays a size x size chess board
     function makeChessBoard() {
         let arr = [];
 
@@ -19,8 +29,14 @@ export default function Board( {size} : Props) {
                 // determine colour of square
                 var colour = ((i + j) % 2 == 0) ? light : dark;
 
-                let coord = "(" + i + ", " + j + ")";
-                row.push(<div key={coord} className={classes} style={colour}></div>);
+                let coordKey = "(" + i + ", " + j + ")";
+                let coord = [j, i];
+                let isSelected = startingCoord[0] === j && startingCoord[1] == i;
+
+                // create square, display image if square is selected
+                row.push(<div key={coordKey} className={classes} style={colour} onClick={() => clickHandler(coord)}>
+                            {isSelected ? <img src={knightColour === "White" ? WhiteKnight : BlackKnight} alt="white knight"/> : null}
+                        </div>);
             }
 
             arr.push(<div key={i}>{row}</div>);
