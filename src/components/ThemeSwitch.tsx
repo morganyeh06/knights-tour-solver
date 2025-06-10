79a8e8272ceb8690dbcba3ad9d508a0bacef55c9
Morgan Yeh <fs.morganyeh@gmail.com>
2025-06-10 18:44:00 -0400

    save selected theme

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -4,29 +4,37 @@ import Sun from "/src/assets/sun.svg";
 import Moon from "/src/assets/moon.svg";
 
 export default function ThemeSwitch() {
-    const [isDark, setIsDark] = useState(false);
+    const [appTheme, setAppTheme] = useState(() => {
+        const theme = localStorage.getItem("theme");
+        return theme ? theme : "light";
+  });
 
     function handleChange() {
-        setIsDark(!isDark);
+        var theme = "";
+
+        if(appTheme === "light") {
+            theme = "dark";
+        } else {
+            theme = "light";
+        }
+
+        setAppTheme(theme);
+        localStorage.setItem("theme", theme);
     }
 
-    useEffect(() => {
-        // css classes to toggle dark mode
-        
-
-        if (isDark) {
+    useEffect(() => {   
+        if (appTheme === "dark") {
             document.documentElement.setAttribute('data-theme', 'dark');
-            
         } else {
             document.documentElement.setAttribute('data-theme', 'light');
         }
-        }, [isDark]);
+        }, [appTheme]);
 
     return (<>
         <div className="theme-toggle">
-            <input type="checkbox" id="theme-switch" checked={isDark} onChange={handleChange}></input>
+            <input type="checkbox" id="theme-switch" checked={appTheme === "dark" ? true : false} onChange={handleChange}></input>
             <label htmlFor="theme-switch">
-                <img src={isDark ? Moon : Sun} alt={isDark ? "moon" : "sun"} className="toggle-img" title="Toggle Light/Dark Mode"></img>
+                <img src={appTheme === "dark" ? Moon : Sun} alt={appTheme === "dark" ? "moon" : "sun"} className="toggle-img" title="Toggle Light/Dark Mode"></img>
             </label>
         </div> 
     </>)
